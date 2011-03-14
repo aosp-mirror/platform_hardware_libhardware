14e3641c32377daabb1e2bf5940957bc9fd04a98
Iliyan Malchev <malchev@google.com>
2011-03-14 14:02:15 -0700

    hardware/libhardware: remove LOCAL_PRELINK_MODULE
    Change-Id: I8a0ba7e4c99e334b9789d88b48383439eb7cc780
    Signed-off-by: Iliyan Malchev <malchev@google.com>

--- modules/gralloc/Android.mk
+++ modules/gralloc/Android.mk
@@ -15,10 +15,10 @@
 
 LOCAL_PATH := $(call my-dir)
 
-# HAL module implemenation, not prelinked and stored in
+# HAL module implemenation stored in
 # hw/<OVERLAY_HARDWARE_MODULE_ID>.<ro.product.board>.so
 include $(CLEAR_VARS)
-LOCAL_PRELINK_MODULE := false
+
 LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/hw
 LOCAL_SHARED_LIBRARIES := liblog libcutils
 
