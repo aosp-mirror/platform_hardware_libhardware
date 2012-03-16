4fd0db7cd15dc01d27d928feb5c7122c908fc70c
Kenny Root <kroot@google.com>
2012-03-16 13:19:07 -0700

    Add tests for keymaster API
    This adds unit tests for implementors of the keymaster API. It can be
    compiled and run as a stand-alone executable.
    
    Change-Id: Id5bb8f4533b744c254b52e07df66a8d158dfda0d

---
+++ tests/keymaster/Android.mk
@@ -0,0 +1,30 @@
+# Build the keymaster unit tests
+
+LOCAL_PATH:= $(call my-dir)
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES:= \
+    keymaster_test.cpp
+
+# Note that "bionic" is needed because of stlport
+LOCAL_C_INCLUDES := \
+    bionic \
+    libcore/include \
+    external/gtest/include \
+    external/stlport/stlport
+
+LOCAL_SHARED_LIBRARIES := \
+    liblog \
+    libutils \
+    libstlport \
+    libhardware
+
+LOCAL_STATIC_LIBRARIES := \
+    libgtest \
+    libgtest_main
+
+LOCAL_MODULE := keymaster_test
+
+LOCAL_MODULE_TAGS := tests
+
+include $(BUILD_EXECUTABLE)
