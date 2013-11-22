ca10795491ce576a66d49103e3270acb3bf9ddfa
Rom Lemarchand <romlem@google.com>
2013-11-22 15:26:48 -0800

    HAL test: statically test the last member of structs
    Ensure the last member of structs is actually the one we expect
    
    Change-Id: Ib623f1a5e09fd9b8d464456528a94742b0ccd00b

--- tests/hardware/Android.mk
+++ tests/hardware/Android.mk
@@ -2,7 +2,7 @@ LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_MODULE := static-hal-check
-LOCAL_SRC_FILES := struct-size.cpp struct-offset.cpp
+LOCAL_SRC_FILES := struct-size.cpp struct-offset.cpp struct-last.cpp
 LOCAL_SHARED_LIBRARIES := libhardware
 LOCAL_CFLAGS := -std=gnu++11 -O0
 
