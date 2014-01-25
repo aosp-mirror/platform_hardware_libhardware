3557abaae2e43428da7559e0a12fb6bd325fab37
Colin Cross <ccross@android.com>
2014-01-24 21:00:32 -0800

    hardware/libhardware: convert LOCAL_MODULE_PATH to LOCAL_MODULE_RELATIVE_PATH
    LOCAL_MODULE_PATH doesn't work for multiarch builds, replace it
    with LOCAL_MODULE_RELATIVE_PATH.
    
    Change-Id: I4e4ceec61d026bbe74ba604554c06104bde42e5e

--- modules/nfc/Android.mk
+++ modules/nfc/Android.mk
@@ -17,7 +17,7 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_MODULE := nfc.default
-LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/hw
+LOCAL_MODULE_RELATIVE_PATH := hw
 LOCAL_SRC_FILES := nfc_pn544_example.c
 LOCAL_SHARED_LIBRARIES := liblog libcutils
 LOCAL_MODULE_TAGS := optional
