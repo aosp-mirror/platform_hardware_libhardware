04d2191800b8fd682c654408c903840a815b611f
Dan Albert <danalbert@google.com>
2014-09-26 10:55:44 -0700

    Don't manually link stlport.
    Change-Id: Ib5a6e31b3a6dea39b45727a49356fa2838ea77bc

--- tests/keymaster/Android.mk
+++ tests/keymaster/Android.mk
@@ -6,26 +6,17 @@ include $(CLEAR_VARS)
 LOCAL_SRC_FILES:= \
     keymaster_test.cpp
 
-# Note that "bionic" is needed because of stlport
 LOCAL_C_INCLUDES := \
-    bionic \
-    external/gtest/include \
     external/openssl/include \
-    external/stlport/stlport
 
 LOCAL_SHARED_LIBRARIES := \
     liblog \
     libutils \
     libcrypto \
-    libstlport \
-    libhardware
-
-LOCAL_STATIC_LIBRARIES := \
-    libgtest \
-    libgtest_main
+    libhardware \
 
 LOCAL_MODULE := keymaster_test
 
 LOCAL_MODULE_TAGS := tests
 
-include $(BUILD_EXECUTABLE)
+include $(BUILD_NATIVE_TEST)
