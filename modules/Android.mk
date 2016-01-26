2c84f81cf07326d9bd0abfb4b14f64bcb583e166
Ruben Brunk <rubenbrunk@google.com>
2016-01-25 23:45:53 -0800

    Add HAL for VR mode.
    - Provide HAL for vendors to turn on VR-specific device performance
      settings when we enter VR mode in VrManagerService.
    
    Bug: 22855417
    Change-Id: I3dee920818eb0988452bf5c25d1a31f11fde7339

--- modules/Android.mk
+++ modules/Android.mk
@@ -1,4 +1,4 @@
 hardware_modules := gralloc hwcomposer audio nfc nfc-nci local_time \
 	power usbaudio audio_remote_submix camera usbcamera consumerir sensors vibrator \
-	tv_input fingerprint input vehicle hardware_properties
+	tv_input fingerprint input vehicle hardware_properties vr
 include $(call all-named-subdir-makefiles,$(hardware_modules))
