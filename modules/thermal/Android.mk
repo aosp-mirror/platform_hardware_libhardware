25cd7f0fb0969683eceb00c9493e2ba8b80228e9
Polina Bondarenko <pbond@google.com>
2016-02-26 12:15:53 +0100

    Re-worked hardware_properties to thermal HAL.
    Bug: 27424857
    Change-Id: Id7212421e499cc33c82e885756d68e89c2317acf

--- modules/thermal/Android.mk
+++ modules/thermal/Android.mk
@@ -16,10 +16,11 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 
-LOCAL_MODULE := hardware_properties.default
+LOCAL_MODULE := thermal.default
 LOCAL_MODULE_RELATIVE_PATH := hw
-LOCAL_SRC_FILES := hardware_properties.c
-LOCAL_SHARED_LIBRARIES := liblog
+LOCAL_SRC_FILES := thermal.c
+LOCAL_SHARED_LIBRARIES := liblog libcutils
 LOCAL_MODULE_TAGS := optional
+LOCAL_CFLAGS := -Wno-unused-parameter
 
 include $(BUILD_SHARED_LIBRARY)
