18782aa54e21ccd23dc53572bc60c53f9d120412
Eino-Ville Talvala <etalvala@google.com>
2016-12-19 16:35:29 -0800

    Camera: Update for service changes, part 2
    - Indicate that HALv3.0 and v3.1 are no longer supported
    - Update tests that reuse camera service plumbing to match
      service code changes
    
    Test: Builds
    Bug: 32991422
    Change-Id: I13b800aea6f65270a2a621bd8a61705160fb2dc8

--- tests/camera2/Android.mk
+++ tests/camera2/Android.mk
@@ -26,9 +26,6 @@ LOCAL_SHARED_LIBRARIES := \
 	libsync \
 	libui \
 	libdl \
-	android.hardware.camera.common@1.0 \
-	android.hardware.camera.provider@2.4 \
-	android.hardware.camera.device@1.0 \
 	android.hardware.camera.device@3.2
 
 LOCAL_C_INCLUDES += \
